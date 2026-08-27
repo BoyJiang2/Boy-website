2e97e9e2676ba04929576212912fa3228c9f6bfb
Boy Jiang <2352027372@qq.com>
2026-08-27 23:18:36 +0800

    Set production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://boy-website.boyjiang2.workers.dev',
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
